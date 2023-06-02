06814a4cacd2b247a5c568d5a0951cc514f3996f
Divy027 <105413340+Divy027@users.noreply.github.com>
2023-06-02 15:56:51 +0530

    Update RewardToken.sol

--- contracts/RewardToken.sol
+++ contracts/RewardToken.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.6;
 
 import "./Openzeppelin/ERC20.sol";
-
+// tokens use to stake
 contract RewardToken is ERC20{
     constructor() ERC20("Reward","RT"){
         _mint(msg.sender,1000000*10**18);
