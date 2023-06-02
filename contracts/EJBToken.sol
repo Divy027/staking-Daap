05c6b79001b1e1eccabea10b0b0f8afe3a90b329
Divy027 <105413340+Divy027@users.noreply.github.com>
2023-06-02 15:56:20 +0530

    Update EJBToken.sol

--- contracts/EJBToken.sol
+++ contracts/EJBToken.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.6;
 import "./Openzeppelin/ERC20.sol";
 
-
+// it is claimed tokens
 contract EarnedToken is ERC20{
     constructor() ERC20("Earned","EJB"){
         _mint(msg.sender,1000000*10**18);
